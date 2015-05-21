5dfabd92b490776be81e4fda6980cd237d733b19
dotnetdude <suresh4job@gmail.com>
2015-05-21 21:13:57 +0000

    addded mvc

--- src/mvc6/Startup.cs
+++ src/mvc6/Startup.cs
@@ -13,14 +13,14 @@ namespace mvc6
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
+            app.UseWelcomePage();
+           
         }
     }
 }
